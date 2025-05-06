1e9670d5e5c2b41fabcfa50389129ee6efaca504
Felipe Grolla Freitas <120500170+Grolla05@users.noreply.github.com>
2025-05-06 18:59:45 -0300

    Update

---
+++ public/E-mail_Contact_us.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('.contact_us form');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault(); // Impede o envio padrão do formulário
+
+        // Obtenha os valores dos campos do formulário
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const phone = document.getElementById('phone').value;
+        const message = document.getElementById('message').value;
+
+        // Parâmetros para o EmailJS
+        const serviceID = 'service_jey5wrw'; // Substitua pelo seu Service ID do EmailJS
+        const templateID = 'template_4xkt3w7'; // Substitua pelo seu Template ID do EmailJS
+        const publicKey = 'CE7O3eEe9VG24eQB4'; // Substitua pela sua Public Key do EmailJS
+
+        // Crie um objeto com os dados a serem enviados
+        const templateParams = {
+            from_name: name,
+            from_email: email,
+            phone: phone,
+            message: message
+        };
+
+        // Envie o email usando o EmailJS
+        emailjs.send(serviceID, templateID, templateParams, publicKey)
+            .then(function (response) {
+                console.log('SUCCESS!', response.status, response.text);
+                alert('Mensagem enviada com sucesso!');
+                form.reset(); // Limpa o formulário após o envio
+            }, function (error) {
+                console.log('FAILED...', error);
+                alert('Ocorreu um erro ao enviar a mensagem. Por favor, tente novamente.');
+            });
+    });
+});
