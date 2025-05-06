2d4602b95c01778f69b82c2a0607f09814db6ecd
Felipe Grolla Freitas <120500170+Grolla05@users.noreply.github.com>
2025-05-06 13:45:47 -0300

    Update (funcionalidade Whatsapp)

---
+++ public/Whatsapp.js
@@ -0,0 +1,9 @@
+// Script JavaScript opcional (pode ser útil para adicionar mais interatividade)
+const whatsappFloatButton = document.querySelector('.whatsapp-float');
+
+if (whatsappFloatButton) {
+    whatsappFloatButton.addEventListener('click', function() {
+        console.log('Botão flutuante do WhatsApp clicado!');
+        // Adicione aqui qualquer outro código que você queira executar ao clicar
+    });
+}
